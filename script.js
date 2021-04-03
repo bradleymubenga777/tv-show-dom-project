f7a9dee0bfadef74530203486ed464e4bf90db68
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-04-03 23:05:18 +0200

    Level 100 Complete

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-//You can edit ALL of the code here
+//Setting The Root Element As A Global Variable
+let root = document.getElementById('root');
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+
+  //Interating Through The Array Of Objects (All Episodes).
+  allEpisodes.forEach(episode => {
+    
+    //Creating An Element (Card) For Each Episode, And Adding Bootstrap Classes & Attributes To It.
+    let episodeCard = document.createElement(`div`);
+    episodeCard.className = `card col-sm-12 col-md-12 col-lg-4 col-xl-4 m-2`;
+    episodeCard.setAttribute(`style`, `width: 18rem`);
+
+    //Setting The Inner HTML Values For The Element Of Each Episode.
+    episodeCard.innerHTML = `
+      <h5 class="card-title text-center pt-2">${episode.name} - S${episode.season}E${episode.number}</h5>
+      <img src="${episode.image[`medium`]}" class="card-img-top img-fluid" alt="${episode.name} Episode Thumbnail">
+      <div class="card-body">
+        ${episode.summary}
+      </div>
+    `;
+
+    //Appending The Created Elements And Values To The Root Element.
+    root.appendChild(episodeCard);
+  });
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+/*   rootElem.textContent = `Got ${episodeList.length} episode(s)`; */
 }
 
 window.onload = setup;
