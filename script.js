eb74fe38abb7d3752bb56b04145dca0b8a617b99
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-04-07 15:45:50 +0200

    Working On Level 200

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Setting The Root Element As A Global Variable
 let root = document.getElementById('root');
+let searchValue = document.getElementById('searchBar');
 
 function setup() {
   const allEpisodes = getAllEpisodes();
@@ -8,22 +9,26 @@ function setup() {
   //Interating Through The Array Of Objects (All Episodes).
   allEpisodes.forEach(episode => {
     
-    //Creating An Element (Card) For Each Episode, And Adding Bootstrap Classes & Attributes To It.
-    let episodeCard = document.createElement(`div`);
-    episodeCard.className = `card col-sm-12 col-md-12 col-lg-4 col-xl-4 m-2`;
-    episodeCard.setAttribute(`style`, `width: 18rem`);
+    //Checking The Value Input Box In Real-time
+    searchValue.addEventListener("input", () => {
+    
+      //Creating An Element (Card) For Each Episode, And Adding Bootstrap Classes & Attributes To It.
+      let episodeCard = document.createElement(`div`);
+      episodeCard.className = `card col-sm-12 col-md-12 col-lg-4 col-xl-4 m-2`;
+      episodeCard.setAttribute(`style`, `width: 18rem`);
 
-    //Setting The Inner HTML Values For The Element Of Each Episode.
-    episodeCard.innerHTML = `
-      <h5 class="card-title text-center pt-2">${episode.name} - S${episode.season}E${episode.number}</h5>
-      <img src="${episode.image[`medium`]}" class="card-img-top img-fluid" alt="${episode.name} Episode Thumbnail">
-      <div class="card-body">
-        ${episode.summary}
-      </div>
-    `;
+      //Setting The Inner HTML Values For The Element Of Each Episode.
+      episodeCard.innerHTML = `
+        <h5 class="card-title text-center pt-2">${episode.name} - S${episode.season}E${episode.number}</h5>
+        <img src="${episode.image[`medium`]}" class="card-img-top img-fluid" alt="${episode.name} Episode Thumbnail">
+        <div class="card-body">
+          ${episode.summary}
+        </div>
+      `;
 
-    //Appending The Created Elements And Values To The Root Element.
-    root.appendChild(episodeCard);
+      //Appending The Created Elements And Values To The Root Element.
+      root.appendChild(episodeCard);
+    })
   });
 }
 
@@ -33,3 +38,4 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+
