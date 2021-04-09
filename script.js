80c242de53f782c920c12e7e501dd49fd4e4e283
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-04-09 12:40:26 +0200

    Level 300 Completed!

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-//Setting The Root Element As A Global Variable
+//Setting The Global Variables
 let root = document.getElementById('root');
 let searchBar = document.getElementById('searchBar');
-let searchTerm = "";
+let searchTerm = '';
+let selectEpisode = '';
 let count = document.getElementById('count');
+let selectElement = document.getElementById('selectEpisodes');
+let episodeLength = 0;
 
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-    
-  //Checking If The Search Bar Is Empty.
-    if (searchBar.value == "") {
-        root.innerHTML = `<h5>TYPE TO START SEARCH</h5>`;
-    }
 
     //Checking The Value Input Box In Real-time.
-    searchBar.addEventListener("input", (event) => {
-        //Setting Our Search Term Value.
-        searchTerm = event.target.value.toLowerCase();
-        showEpisodes();
+    searchBar.addEventListener('input', (event) => {
+      //Setting Our Search Term Value.
+      searchTerm = event.target.value.toLowerCase();
+      showEpisodes();
     });
 
+    selectElement.addEventListener('change', (event) => {
+      //Setting The Value Of Our Search Term.
+      selectEpisode = event.target.value.toLowerCase();
+      console.log(selectEpisode);
+      selectEpisodes();
+    });
+
+
+    //Search Bar Function
     const showEpisodes = () => {
       //Clearing All Elements In The Root.
       root.innerHTML = ``;
-      
-      //Making Our Episode Number Variable Acessesible Within This Scope.
-      let episodeLength = 0;
+      episodeLength = 0;
 
       //Filtering Through All Episodes To See If It Matches Search.
       allEpisodes
@@ -44,6 +49,10 @@ function setup() {
         let episodeCard = document.createElement(`div`);
         episodeCard.className = `card col-sm-12 col-md-12 col-lg-4 col-xl-4 m-2`;
         episodeCard.setAttribute(`style`, `width: 18rem`);
+        
+        //Creating An Option For Each Episode.
+        let selectItem = document.createElement(`option`);
+        selectItem.setAttribute(`value`, `${episode.name}`);
             
         //Setting The Inner HTML Values For The Element Of Each Episode.
         episodeCard.innerHTML = `
@@ -53,14 +62,104 @@ function setup() {
             ${episode.summary}
           </div>
           `;
+
+        selectItem.innerText = `${episode.name}`;
             
         //Appending The Created Elements And Values To The Root Element.
         root.appendChild(episodeCard);
+        selectElement.appendChild(selectItem);
 
         //Setting The Episode Count Value
         count.innerText = `Displaying ${episodeLength}/73 Episodes`;
       });
     };
+
+    //Select Element Function
+    const selectEpisodes = () => {
+      //Clearing All Elements In The Root.
+      root.innerHTML = ``;
+      episodeLength = 0;
+
+      //Filtering Through All Episodes To See If It Matches Search.
+      allEpisodes
+      .filter((by) => {
+        return (
+          by.name.toLowerCase().includes(selectEpisode) || by.summary.toLowerCase().includes(selectEpisode)
+        )
+      })
+      //Then We Take The Result, Loop Through It And Show It In Our DOM.
+      .forEach((episode) => {
+        //For Each Episode Increment Episode Number
+        episodeLength++;
+        
+        //Creating An Element (Card) For Each Episode, And Adding Bootstrap Classes & Attributes To It.
+        let episodeCard = document.createElement(`div`);
+        episodeCard.className = `card col-sm-12 col-md-12 col-lg-4 col-xl-4 m-2`;
+        episodeCard.setAttribute(`style`, `width: 18rem`);
+        
+        //Creating An Option For Each Episode.
+        let selectItem = document.createElement(`option`);
+        selectItem.setAttribute(`value`, `${episode.name}`);
+            
+        //Setting The Inner HTML Values For The Element Of Each Episode.
+        episodeCard.innerHTML = `
+          <h5 class="card-title text-center pt-2">${episode.name} - S${episode.season}E${episode.number}</h5>
+          <img src="${episode.image[`medium`]}" class="card-img-top img-fluid" alt="${episode.name} Episode Thumbnail">
+          <div class="card-body">
+            ${episode.summary}
+          </div>
+          `;
+
+        selectItem.innerText = `${episode.name}`;
+            
+        //Appending The Created Elements And Values To The Root Element.
+        root.appendChild(episodeCard);
+        selectElement.appendChild(selectItem);
+
+        //Setting The Episode Count Value
+        count.innerText = `Displaying ${episodeLength}/73 Episodes`;
+      });
+    }
+
+    const showEverything = () => {
+      //Clearing All Elements In The Root.
+      root.innerHTML = ``;
+      episodeLength = 0;
+      allEpisodes.forEach(episode => {
+        //For Each Episode Increment Episode Number
+        episodeLength++;
+        
+        //Creating An Element (Card) For Each Episode, And Adding Bootstrap Classes & Attributes To It.
+        let episodeCard = document.createElement(`div`);
+        episodeCard.className = `card col-sm-12 col-md-12 col-lg-4 col-xl-4 m-2`;
+        episodeCard.setAttribute(`style`, `width: 18rem`);
+        
+        //Creating An Option For Each Episode.
+        let selectItem = document.createElement(`option`);
+        selectItem.setAttribute(`value`, `${episode.name}`);
+            
+        //Setting The Inner HTML Values For The Element Of Each Episode.
+        episodeCard.innerHTML = `
+          <h5 class="card-title text-center pt-2">${episode.name} - S${episode.season}E${episode.number}</h5>
+          <img src="${episode.image[`medium`]}" class="card-img-top img-fluid" alt="${episode.name} Episode Thumbnail">
+          <div class="card-body">
+            ${episode.summary}
+          </div>
+          `;
+
+        selectItem.innerText = `${episode.name}`;
+            
+        //Appending The Created Elements And Values To The Root Element.
+        root.appendChild(episodeCard);
+        selectElement.appendChild(selectItem);
+
+        //Setting The Episode Count Value
+        count.innerText = `Displaying ${episodeLength}/73 Episodes`;
+      })
+    }
+
+    //Checking If The Search Bar Is Empty.
+    showEverything();
   }
 
 
